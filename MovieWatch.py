ca4a974ddecbaca04c30c8c775ff90630b6d1310
Alexandria Curtis <alexandria@Alexandrias-MacBook-Pro-2.local>
2019-07-19 10:14:15 -0500

    Create IMDB Client class to wrap code

--- MovieWatch.py
+++ MovieWatch.py
@@ -2,21 +2,59 @@ import urllib3
 from bs4 import BeautifulSoup
 
 
-year = "2019"
 
-url = "http://www.imdb.com/search/title?release_date=" + year + "," + year + "&title_type=feature"
-dataUrl = urllib3.PoolManager().request('GET', url).data
-soup = BeautifulSoup(dataUrl, "html.parser")
+class IMDBClient:
+
+    def __init__(self):
+        self.url = "http://www.imdb.com/search/title?"
 
 
-i = 1
-movieList = soup.findAll('div', attrs={'class': 'lister-item mode-advanced'})
+    def searchByReleaseYear(self, releaseYear):
 
-for divItem in movieList:
-    div = divItem.find('div', attrs={'class': 'lister-item-content'})
+        print("Searching by release year: ", releaseYear)
+        searchUrl = self.url + "release_date=" + releaseYear
 
-    header = div.findChildren('h3', attrs={'class':'lister-item-header'})
+        dataUrl = urllib3.PoolManager().request('GET', searchUrl).data
+        soup = BeautifulSoup(dataUrl, "html.parser")
 
-    print(str(i) + '.', 'Movie: ' + str((header[0].findChildren('a'))[0].contents[0].encode('utf-8').decode('ascii', 'ignore')))
+        i = 1
+        movieList = soup.findAll('div', attrs={'class': 'lister-item mode-advanced'})
 
-    i += 1
+        for divItem in movieList:
+            div = divItem.find('div', attrs={'class': 'lister-item-content'})
+
+            header = div.findChildren('h3', attrs={'class': 'lister-item-header'})
+
+            print(str(i) + '.', 'Movie: ' + str(
+                (header[0].findChildren('a'))[0].contents[0].encode('utf-8').decode('ascii', 'ignore')))
+
+            i += 1
+        print('\n')
+
+    def searchByTitle(self, title):
+
+        print("Searching by title: ", title)
+        searchUrl = self.url + "title=" + title
+
+        dataUrl = urllib3.PoolManager().request('GET', searchUrl).data
+        soup = BeautifulSoup(dataUrl, "html.parser")
+
+        i = 1
+        movieList = soup.findAll('div', attrs={'class': 'lister-item mode-advanced'})
+
+        for divItem in movieList:
+            div = divItem.find('div', attrs={'class': 'lister-item-content'})
+
+            header = div.findChildren('h3', attrs={'class': 'lister-item-header'})
+
+            print(str(i) + '.', 'Movie: ' + str(
+                (header[0].findChildren('a'))[0].contents[0].encode('utf-8').decode('ascii', 'ignore')))
+
+            i += 1
+        print('\n')
+
+
+
+client = IMDBClient()
+client.searchByReleaseYear("2015")
+client.searchByTitle("Fear")
